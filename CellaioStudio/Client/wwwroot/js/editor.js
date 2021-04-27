995419d325606fe4511dbb8ad6b3309899f57ab9
Paweł Piorun <dev.pawelpiorun@gmail.com>
2021-04-27 22:03:38 +0200

    Show example project, remove sealing for now

--- CellaioStudio/Client/wwwroot/js/editor.js
+++ CellaioStudio/Client/wwwroot/js/editor.js
@@ -4,9 +4,9 @@ import { OrbitControls } from "../js/OrbitControls.js"
 var container;
 var scene, camera, renderer, controls;
 
-var geometry, material, cube;
-
 var floor1, floor2, sealing1, sealing2, frontWall, sideWall, backWall;
+var shelves;
+const shelveThickness = 0.0682;
 
 function render() {
     controls.update();
@@ -27,7 +27,7 @@ function loadScene() {
 
     scene = new THREE.Scene();
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(container.clientWidth, container.clientHeight);
     container.appendChild(renderer.domElement);
@@ -96,17 +96,17 @@ function loadRoom() {
     scene.add(frontWall);
     scene.add(sideWall);
     scene.add(backWall);
-    scene.add(sealing1);
-    scene.add(sealing2);
+    //scene.add(sealing1);
+    //scene.add(sealing2);
 }
 
 function createWall(x, y) {
 
-    var wallTexture = new THREE.TextureLoader().load('wallplates.jpg');
+    var wallTexture = new THREE.TextureLoader().load('textures/wallplates.jpg');
     wallTexture.wrapS = THREE.RepeatWrapping;
     wallTexture.wrapT = THREE.RepeatWrapping;
     wallTexture.repeat.set(2 / 3 * x, 2 / 3 * y);
-    var wallMaterial = new THREE.MeshLambertMaterial({ map: wallTexture });
+    var wallMaterial = new THREE.MeshPhongMaterial({ map: wallTexture });
     wallMaterial.side = THREE.DoubleSide;
 
     return new THREE.Mesh(new THREE.PlaneGeometry(x, y), wallMaterial);
@@ -131,11 +131,10 @@ function loadLighting() {
 
 function addShelve() {
 
-    geometry = new THREE.BoxGeometry(1, 1, 1);
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    cube = new THREE.Mesh(geometry, material);
+    var shv = createShelve(1, 0.4);
+    shv.position.set(0.75, 1, 0.2);
 
-    scene.add(cube);
+    scene.add(shv);
 }
 
 function handleResizing() {
@@ -149,8 +148,61 @@ function handleResizing() {
     });
 }
 
+var shelveTexture = new THREE.TextureLoader().load('textures/wood2.jpg');
+function createShelve(length, depth = 0.4, thickness = shelveThickness) {
+    var geometry = new THREE.BoxGeometry(length, thickness, depth);
+    var material = new THREE.MeshPhongMaterial({ map: shelveTexture });
+    var cube = new THREE.Mesh(geometry, material);
+    return cube;
+}
+
+function exampleProject() {
+
+    // 1374 from bottom to top
+    var s1 = createShelve(1.3);
+    var s2 = createShelve(1.7);
+    var s3 = createShelve(1.7);
+    var s4 = createShelve(1.7);
+    var s5 = createShelve(1.3);
+    s1.position.set(1.45, 0.5, 0.2);
+    s2.position.set(1.65, 0.9, 0.2);
+    s3.position.set(1.65, 1.3, 0.2);
+    s4.position.set(1.65, 1.7, 0.2);
+    s5.position.set(1.85, 2.1, 0.2);
+
+    var h1 = createShelve(0.4 - shelveThickness);
+    var h2 = createShelve(0.4 - shelveThickness);
+    var h3 = createShelve(0.4 - shelveThickness);
+    var h4 = createShelve(0.4 - shelveThickness);
+    var h5 = createShelve(0.4 - shelveThickness);
+    var h6 = createShelve(0.4 - shelveThickness);
+    var h7 = createShelve(0.4 - shelveThickness);
+    var h8 = createShelve(0.4 - shelveThickness);
+
+    h1.rotateZ(Math.PI / 2);
+    h2.rotateZ(Math.PI / 2);
+    h3.rotateZ(Math.PI / 2);
+    h4.rotateZ(Math.PI / 2);
+    h5.rotateZ(Math.PI / 2);
+    h6.rotateZ(Math.PI / 2);
+    h7.rotateZ(Math.PI / 2);
+    h8.rotateZ(Math.PI / 2);
+    h1.position.set(0.8 + shelveThickness / 2, 0.7, 0.2);
+    h2.position.set(1.2 + shelveThickness / 2, 0.7, 0.2);
+    h3.position.set(2.1 - shelveThickness / 2, 1.1, 0.2);
+    h4.position.set(2.5 - shelveThickness / 2, 1.1, 0.2);
+    h5.position.set(0.8 + shelveThickness / 2, 1.5, 0.2);
+    h6.position.set(1.2 + shelveThickness / 2, 1.5, 0.2);
+    h7.position.set(2.1 - shelveThickness / 2, 1.9, 0.2);
+    h8.position.set(2.5 - shelveThickness / 2, 1.9, 0.2);
+
+    scene.add(s1, s2, s3, s4, s5);
+    scene.add(h1, h2, h3, h4, h5, h6, h7, h8);
+}
+
 window.editor = {
     load: () => { loadScene(); },
+    exampleProject: () => { exampleProject(); },
     addShelve: () => { addShelve(); }
 }
 
