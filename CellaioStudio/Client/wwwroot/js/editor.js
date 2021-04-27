1e855f486917c8169dc4dc02c3c690c822a03300
Paweł Piorun <dev.pawelpiorun@gmail.com>
2021-04-27 21:02:13 +0200

    Prepare the scene (room)

--- CellaioStudio/Client/wwwroot/js/editor.js
+++ CellaioStudio/Client/wwwroot/js/editor.js
@@ -6,6 +6,8 @@ var scene, camera, renderer, controls;
 
 var geometry, material, cube;
 
+var floor1, floor2, sealing1, sealing2, frontWall, sideWall, backWall;
+
 function render() {
     controls.update();
     renderer.render(scene, camera);
@@ -24,19 +26,107 @@ function loadScene() {
     }
 
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 1, 1000);
+
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(container.clientWidth, container.clientHeight);
     container.appendChild(renderer.domElement);
 
+    camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 1, 1000);
+    camera.rotateZ(Math.PI / 2);
+    camera.position.z = 3;
+    camera.position.x = -0.5;
+    camera.position.y = 1.5;
+
     controls = new OrbitControls(camera, renderer.domElement);
-    
-    camera.position.z = 5;
+    controls.target.set(1.2, 1.4, 0);
+    // todo: controls - enableDamping?
+    loadRoom();
+    loadLighting();
+
+    //const axesHelper = new THREE.AxesHelper(5);
+    //scene.add(axesHelper);
 
     handleResizing();
 
     animate();
+}
+
+function loadRoom() {
+    // todo: onLoad?
+
+    frontWall = createWall(3, 2.5);
+    frontWall.position.y = 2.5 / 2;;
+    frontWall.position.x = 3 / 2;
+
+    floor1 = createFloor(6, 5);
+    floor1.rotateX(Math.PI / 2);
+    floor1.position.z = 2.5;
+
+    floor2 = createFloor(3, 5);
+    floor2.rotateX(Math.PI / 2);
+    floor2.position.x = -3 / 2;
+    floor2.position.z = -2.5;
+
+    sideWall = createWall(3, 2.5);
+    sideWall.rotateY(Math.PI / 2);
+    sideWall.position.x = 3;
+    sideWall.position.y = 2.5 / 2;
+    sideWall.position.z = 3 / 2;
+
+    backWall = createWall(5, 2.5);
+    backWall.rotateY(Math.PI / 2);
+    backWall.position.y = 2.5 / 2;
+    backWall.position.z = -2.5;
+
+    sealing1 = createWall(6, 5);
+    sealing1.rotateX(Math.PI / 2);
+    sealing1.position.y = 2.5;
+    sealing1.position.z = 2.5;
+
+    sealing2 = createWall(3, 5);
+    sealing2.rotateX(Math.PI / 2);
+    sealing2.position.y = 2.5;
+    sealing2.position.z = -2.5;
+    sealing2.position.x = -1.5;
+    
+
+    scene.add(floor1);
+    scene.add(floor2);
+    scene.add(frontWall);
+    scene.add(sideWall);
+    scene.add(backWall);
+    scene.add(sealing1);
+    scene.add(sealing2);
+}
+
+function createWall(x, y) {
+
+    var wallTexture = new THREE.TextureLoader().load('wallplates.jpg');
+    wallTexture.wrapS = THREE.RepeatWrapping;
+    wallTexture.wrapT = THREE.RepeatWrapping;
+    wallTexture.repeat.set(2 / 3 * x, 2 / 3 * y);
+    var wallMaterial = new THREE.MeshLambertMaterial({ map: wallTexture });
+    wallMaterial.side = THREE.DoubleSide;
+
+    return new THREE.Mesh(new THREE.PlaneGeometry(x, y), wallMaterial);
+}
+
+function createFloor(x, y) {
+    // todo
+    return createWall(x, y);
+}
+
+var light, ambientLight;
+function loadLighting() {
+
+    ambientLight = new THREE.AmbientLight(0xffeedd, 0.2);
+    light = new THREE.PointLight(0xffffff, 1, 25);
+    light.position.set(2, 2, 2);
+    scene.add(light);
+    scene.add(ambientLight);
+
+    scene.fog = new THREE.Fog(0x000000, 2, 7);
 }
 
 function addShelve() {
